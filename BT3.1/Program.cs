8ef79514e2c950b77b2d337f44f409c685ddbe01
Le Minh Dungx <leminhdung050@gmail.com>
2026-08-27 16:27:19 +0700

    Add files via upload

---
+++ BT3.1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT3._1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Nhap so thu nhat a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so thu hai b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap phep toan (+, -, *, /, %): ");
+            char op = char.Parse(Console.ReadLine());
+
+            double result;
+
+            switch (op)
+            {
+                case '+':
+                    result = a + b;
+                    Console.WriteLine("\nKet qua: " + result);
+                    break;
+                case '-':
+                    result = a - b;
+                    Console.WriteLine("\nKet qua: " + result);
+                    break;
+                case '*':
+                    result = a * b;
+                    Console.WriteLine("\nKet qua: " + result);
+                    break;
+                case '/':
+                    if (b != 0)
+                    {
+                        result = a / b;
+                        Console.WriteLine("\nKet qua: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nLoi: Khong the chia cho 0.");
+                    }
+                    break;
+                case '%':
+                    if (b != 0)
+                    {
+                        result = a % b;
+                        Console.WriteLine("\nKet qua: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nLoi: Khong the chia cho 0.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("\nLoi: Phep toan khong hop le.");
+                    break;
+            }
+
+        }
+    }
+}
